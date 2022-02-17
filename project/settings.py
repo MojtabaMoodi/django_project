2e808580c42b737532becd439feb84eb9a13ccaf
MojtabaMoodi <mmoodi@uwaterloo.ca>
2022-02-17 18:23:11 -0500

    django-heroku ignore secret key management.

--- project/settings.py
+++ project/settings.py
@@ -136,7 +136,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
-django_heroku.settings(locals())
+django_heroku.settings(locals(), secret_key=False)
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
