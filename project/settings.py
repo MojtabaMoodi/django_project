c132ed42a9efa43ced18a447c905f25f00e92fb0
MojtabaMoodi <mmoodi@uwaterloo.ca>
2022-02-18 23:38:21 -0500

    Create the users app and modify the User model.

--- project/settings.py
+++ project/settings.py
@@ -49,6 +49,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'homeschool.users',
 ]
 
 MIDDLEWARE = [
@@ -117,6 +118,8 @@ AUTH_PASSWORD_VALIDATORS = [
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
 ]
+
+AUTH_USER_MODEL = 'users.User'
 
 
 # Internationalization
